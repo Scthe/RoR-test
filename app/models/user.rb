23f6320b02629cbc58460f83387fef0c89c997bd
Scthe <marcin1113C@gmail.com>
2014-09-04 00:13:05 +0200

    Add user validation with tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,19 @@
+# validates :username, :uniqueness => true
+# validates :username, uniqueness: true
+
 class User < ActiveRecord::Base
+	MALE = { :value => 0, :display_name => "Male"}
+	FEMALE = { :value => 1, :display_name => "Female"}
+	
+	validates :username, uniqueness: true, presence: true, length: { minimum: 3}, format: { with: /\A[0-9A-Za-z_]+\Z/i, message: "letters/numbers !" }
+	validates :firstname, allow_blank: true, length: { minimum: 2}, format: { with: /\A[A-Za-z_ ]+\Z/i, message: "letters !" }
+	validates :lastname, allow_blank: true, length: { minimum: 2}, format: { with: /\A[A-Za-z_ ]+\Z/i, message: "letters !" }
+	validates :gender, inclusion: { in: [ MALE[:value], FEMALE[:value] ]}
+=begin
+	validates :email, presence true, uniqueness true, length { minimum: 3}, email true
+	validates :password, presence true,  length { minimum: 3}
+	#t.boolean :is_active
+    #t.date :birthdate
+=end
+
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,77 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "correct user should be valid" do
+    u = create_ok_user
+    assert u.valid?
+  end
+
+  test "should not be valid with incorrect username" do
+    u = create_ok_user
+
+    u.username = nil
+    assert_not u.valid?
+
+    u.username = "Aa"
+    assert_not u.valid?
+
+    u.username = "UserAbc$"
+    assert_not u.valid?
+  end
+
+  test "should allow blank first and lastname" do
+    u = create_ok_user
+    u.lastname = nil
+    u.firstname = nil
+    assert u.valid?
+  end
+
+  test "should not be valid with incorrect first and lastname" do
+    u = create_ok_user
+    u.lastname = "a"
+    assert_not u.valid?
+
+    u = create_ok_user
+    u.firstname = "a"
+    assert_not u.valid?
+
+    u = create_ok_user
+    u.lastname = "Aaa9"
+    assert_not u.valid?
+
+    u = create_ok_user
+    u.firstname = "Aaa9"
+    assert_not u.valid?
+  end
+
+  test "gender should be valid only in 2 cases ( oh, wow !)" do
+    test = {
+      User::MALE[:value] => true,
+      User::FEMALE[:value] => true,
+      -1 => false,
+      2 => false
+    }
+    test.each do |k,v|
+      u = create_ok_user
+      u.gender = k
+      assert ( u.valid? == v)
+    end
+  end
+
+  # TODO validate email & password
+
+  private
+  def create_ok_user
+    # TODO use fixtures
+    User.new.tap do |u|
+      u.username = "User1"
+      u.firstname = "John"
+      u.lastname = "Smith"
+      u.gender = 1
+      u.email = "a.smith@gmail.com"
+      u.password = "pass111"
+    end
+  end
+
 end
