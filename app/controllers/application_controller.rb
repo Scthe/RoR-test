1036ec0f8cf6ee4cd9ab7e0f61a3a5c7c42c9748
Scthe <marcin1113C@gmail.com>
2014-09-08 22:47:28 +0200

    Base layout - with css
    Signed-off-by: Scthe <marcin1113C@gmail.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
 	protect_from_forgery with: :exception
 
 	def index
-		# render "hi !"
+		# TODO use jade
 		@user = stub_user
 		render layout: true
 	end
