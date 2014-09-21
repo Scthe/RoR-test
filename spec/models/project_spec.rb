28b79c74138990992ef6add5c0cbcaa20f95802a
Scthe <marcin1113C@gmail.com>
2014-09-21 15:24:31 +0200

    Transfer other model tests

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,1 +1,45 @@
 require 'rails_helper'
+
+RSpec.describe Project, :type => :model do
+
+	before(:each) do
+		@project = build(:project_a)
+	end
+
+
+	context "validation" do
+
+		it "default fixture should pass" do
+			expect(@project.valid?).to be true
+		end
+
+		it "should have proper name" do
+			@project.name = ""
+			expect(@project.valid?).to be false
+
+			@project.name = nil
+			expect(@project.valid?).to be false
+
+			@project.name = "Aasd!!!"
+			expect(@project.valid?).to be false
+
+			@project.name = "aaaaaBBBBBaaaaaBBBBBaaaaaBBBBBaaaaaBBBBBaaaaaBBBBBa"
+			expect(@project.valid?).to be false
+		end
+
+		it "complete status should be limited to [0, 100]" do
+			@project.complete = 0
+			expect(@project.valid?).to be true
+
+			@project.complete = 100
+			expect(@project.valid?).to be true
+
+			@project.complete = -1
+			expect(@project.valid?).to be false
+
+			@project.complete = 101
+			expect(@project.valid?).to be false
+		end
+	end
+
+end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,1 +1,45 @@
 require 'rails_helper'
+
+RSpec.describe Task, :type => :model do
+
+	before(:each) do
+		@task  = build(:task_a)
+		@task2 = build(:task_b)
+	end
+
+
+	context "validation" do
+
+		it "default fixture should pass" do
+			expect(@task.valid?).to be true
+			expect(@task2.valid?).to be true
+		end
+
+		it "should have correct title" do
+			@task.title = "a"
+			expect(@task.valid?).to be false
+
+			@task.title = "a"
+			expect(@task.valid?).to be false
+
+			@task.title = "a" * 50
+			expect(@task.valid?).to be true
+
+			@task.title = "a" * 51
+			expect(@task.valid?).to be false
+		end
+
+		it "should have correct type" do
+			Task::TASK_TYPES.each do |t|
+				@task.task_type = t
+				expect(@task.valid?).to be true
+			end
+
+			@task.task_type = -1
+			expect(@task.valid?).to be false
+
+			@task.task_type = 4
+			expect(@task.valid?).to be false
+		end
+	end
+end
